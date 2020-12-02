371315f947ac6623e6914b18ae8b89e3be069b49
TMO <morotmk@gmail.com>
2020-12-02 20:46:07 +0100

    DrinkyWinkey console app

---
+++ src/main/java/com/infoshare/drinkywinky/properties/AppConfig.java
@@ -0,0 +1,10 @@
+package com.infoshare.drinkywinky.properties;
+
+public class AppConfig {
+
+    public static Object RECIPE_SORT_TYPE;
+    public static String DATA_FORMAT;
+
+    AppConfig() {
+    }
+}
